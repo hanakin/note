5850da2e4739abb5980b8e78b6c9e44ad6cb5d2f
Michael Miday <midaym@gmail.com>
2015-09-13 10:33:47 +0200

    test app.js and server

--- src/app.js
+++ src/app.js
@@ -4,8 +4,14 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function(request, response){
-	response.send("Hello World!")
+app.get('/', function(req, res){
+	res.send("<h1>I am Alive!!</h1>")
 });
 
-app.listen(3000);
+app.get('/blog', function(req, res){
+	res.send("The Blog")
+})
+
+app.listen(3000, function(){
+	console.log("The frontend server is running on port 3000!")
+});
